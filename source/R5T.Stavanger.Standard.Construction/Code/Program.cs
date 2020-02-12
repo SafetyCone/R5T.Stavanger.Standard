33173f432771e853d6c220a9a51ce1363e6e2c6f
MinexAutomation <minex.automation@gmail.com>
2020-02-11 22:44:27 -0800

    Added Startup and Program-as-service functionality.

--- source/R5T.Stavanger.Standard.Construction/Code/Program.cs
+++ source/R5T.Stavanger.Standard.Construction/Code/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Chalandri;
+using R5T.Evosmos;
+using R5T.Richmond;
+
+
 namespace R5T.Stavanger.Standard.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var serviceProvider = ServiceProviderBuilder.NewUseStartup<Startup>() as ServiceProvider)
+            {
+                var program = serviceProvider.GetRequiredService<Program>();
+
+                program.Run();
+            }
         }
+
+
+        private void Run()
+        {
+            var shortcutTargetFilePath = this.TestingDataDirectoryContentPathsProvider.GetBasicTextFilePath();
+
+            var shortcutFilePath = this.TemporaryDirectoryFilePathProvider.GetTemporaryDirectoryFilePath("Testing Text.txt");
+
+            this.ShortcutOperator.CreateShortcut(shortcutFilePath, shortcutTargetFilePath);
+        }
+
+
+        #region Services and Constructor
+
+        private IShortcutOperator ShortcutOperator { get; }
+        private ITestingDataDirectoryContentPathsProvider TestingDataDirectoryContentPathsProvider { get; }
+        private ITemporaryDirectoryFilePathProvider TemporaryDirectoryFilePathProvider { get; }
+
+
+        public Program(
+            IShortcutOperator shortcutOperator,
+            ITestingDataDirectoryContentPathsProvider testingDataDirectoryContentPathsProvider,
+            ITemporaryDirectoryFilePathProvider temporaryDirectoryFilePathProvider)
+        {
+            this.ShortcutOperator = shortcutOperator;
+            this.TestingDataDirectoryContentPathsProvider = testingDataDirectoryContentPathsProvider;
+            this.TemporaryDirectoryFilePathProvider = temporaryDirectoryFilePathProvider;
+        }
+
+        #endregion
     }
 }
---
+++ source/R5T.Stavanger.Standard.Construction/Code/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Richmond;
+
+using R5T.Stavanger.Default;
+
+
+namespace R5T.Stavanger.Standard.Construction
+{
+    public class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger) : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddShortcutOperator()
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddTemporaryDirectoryFilePathProvider()
+                ;
+        }
+    }
+}
